9d4d2b4c43031d2e077eef18eb9e4aa7f78b8efb
Sarangan Rajamanickam <sarajama@microsoft.com>
2021-06-11 01:12:32 -0700

    Integration Test Generated Changes

--- test/integration/generated/arrayConstraints/src/arrayConstraintsClientContext.ts
+++ test/integration/generated/arrayConstraints/src/arrayConstraintsClientContext.ts
@@ -39,6 +39,15 @@ export class ArrayConstraintsClientContext extends coreClient.ServiceClient {
       requestContentType: "application/json; charset=utf-8"
     };
 
+    const userAgentPrefix =
+      options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+        ? `${options.userAgentOptions.userAgentPrefix} array-constraints-client/1.0.0-preview1`
+        : `array-constraints-client/1.0.0-preview1`;
+
+    options.userAgentOptions = {
+      userAgentPrefix: userAgentPrefix
+    };
+
     const optionsWithDefaults = {
       ...defaults,
       ...options,
